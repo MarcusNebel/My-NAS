8ca1c087c4cc4e59b6459b3c3f36134b3c79bca5
Marcus Nebel <marcusnebel09@gmail.com>
2025-02-18 14:37:08 +0100

    Changed images

--- Login/script.js
+++ Login/script.js
@@ -6,18 +6,7 @@ const backgroundImages = [
   'images/image5.jpg',
   'images/image6.jpg',
   'images/image7.jpg',
-  'images/image8.jpg',
-  'images/image9.jpg',
-  'images/image10.jpg',
-  'images/image11.jpg',
-  'images/image12.jpg',
-  'images/image13.jpg',
-  'images/image14.jpg',
-  'images/image15.jpg',
-  'images/image16.jpg',
-  'images/image17.jpg',
-  'images/image18.jpg',
-  'images/image19.jpg'
+  'images/image8.jpg'
 ];
 
 const randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];

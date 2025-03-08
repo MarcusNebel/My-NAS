2c3f7c7dd07af69a7663dbe72ec52e8378c98a17
Marcus Nebel <marcusnebel09@gmail.com>
2025-03-08 13:22:37 +0100

    Fixed My NAS Logo design issue

--- File_upload.php
+++ File_upload.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION["username"])) {
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;600;700;900&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,600;0,700;0,900;1,400;1,600;1,700&display=swap" rel="stylesheet" />
     
     <link rel="stylesheet" href="assets/css/style.css" />
 </head>
